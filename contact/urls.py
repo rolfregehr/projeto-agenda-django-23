cffdf8e77a2153058bf0ee4858d9951ddf085b2f
Rolf <rolf.regehr@gmail.com>
2024-11-27 15:15:03 -0300

    Filtrando valores com Q e OR para o campo de pesquisa

--- contact/urls.py
+++ contact/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from contact import views
 
+app_name = 'contact'
+
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
+    path('search/', views.search, name='search'),  # type:ignore
+    path('', views.index, name='index'),  # type:ignore
 ]
