28b0cca8e80ad7c7e5f5267c70d7edca02f73fd1
Rolf <rolf.regehr@gmail.com>
2024-12-04 21:48:17 -0300

    Validando campos de first_name, last_name e email do Usuário

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from contact.forms import RegisterForm
+
 def register(request):
     form = RegisterForm()
+
+   
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+
         if form.is_valid():
             form.save()
+            messages.success(request, 'Usuário registrado')
+            return redirect('contact:index')
+
     return render(
         request,
         'contact/register.html',
